005bd104a013d580fdf3eecb9c7df42f41efdc29
UnderBridgeKid97 <waktaverse24@gmail.com>
2024-10-07 17:21:59 +0900

    10.07
    1) 플레이어 캐스팅, 충돌체와 거리 구하기
    2) 문열기 인터렉션 기능 구현

---
+++ Assets/MyFPS/Scripts/DoorCellOpen.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+namespace MyFps
+{
+    public class DoorCellOpen : MonoBehaviour
+    {
+        #region Variables
+        public GameObject ActionUI;
+        public  TextMeshProUGUI actionText;
+        [SerializeField]private string action = "Open The Door";
+
+        private float theDistance;
+
+        private Animator animator;
+        private Collider collider;
+        public AudioSource audioSource;
+        #endregion
+
+        private void Start()
+        {
+            animator = GetComponent<Animator>();
+            collider = GetComponent<Collider>();
+        }
+        private void Update()
+        {
+            theDistance = PlayerCasting.distanceFromTarget;
+        }
+        // 마우스를 가져가면 액션ui를 보여준다
+        private void OnMouseOver()
+        {
+            // 거리가 2이하일때
+            if (theDistance <= 2f)
+            {
+                ActionUI.SetActive(true);
+                actionText.text = action;
+
+                // 문이 열리는 액션
+                if(Input.GetButtonDown("Action"))
+                {
+                    animator.SetBool("IsOpen", true);
+                    collider.enabled = false;
+                    audioSource.Play();
+                }
+                
+            }
+            else
+            {
+                HideActionUI();
+            }
+           
+        }
+
+        // 마우스가 벗어나면 액션ui를 감춘다
+        private void OnMouseExit()
+        {
+            HideActionUI();
+        }
+
+        void HideActionUI()
+        {
+            ActionUI.SetActive(false);
+            actionText.text = "";
+        }
+
+    }
+}
